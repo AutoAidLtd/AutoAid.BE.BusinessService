5ec0cb26e4c0a70168f66acfdc6374eff30f9884
khoabui <buidangkhoa05@gmail.com>
2024-01-22 11:28:54 +0700

    (..) rename folder

--- Source/AutoAid.Domain/Common/AppConfig.cs
+++ Source/AutoAid.Domain/Common/AppConfig.cs
@@ -19,46 +19,14 @@
     }
     public class JwtSetting
     {
-        public bool ValidateIssuerSigningKey
-        {
-            get;
-            set;
-        }
-        public string IssuerSigningKey
-        {
-            get;
-            set;
-        }
-        public bool ValidateIssuer
-        {
-            get;
-            set;
-        } = true;
-        public string ValidIssuer
-        {
-            get;
-            set;
-        }
-        public bool ValidateAudience
-        {
-            get;
-            set;
-        } = true;
-        public string ValidAudience
-        {
-            get;
-            set;
-        }
-        public bool RequireExpirationTime
-        {
-            get;
-            set;
-        }
-        public bool ValidateLifetime
-        {
-            get;
-            set;
-        } = true;
+        public bool ValidateIssuerSigningKey { get; set; }
+        public string IssuerSigningKey { get; set; }
+        public bool ValidateIssuer { get; set; } = true;
+        public string ValidIssuer { get; set; }
+        public bool ValidateAudience { get; set; } = true;
+        public string ValidAudience { get; set; }
+        public bool RequireExpirationTime { get; set; }
+        public bool ValidateLifetime { get; set; } = true;
     }
     public class AwsCredentials
     {
